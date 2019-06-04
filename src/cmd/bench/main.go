6c3f4ff34403f2da172e8f7671ce02e8f6c2b265
Richard Artoul <rartoul@uber.com>
2019-06-03 21:02:39 -0400

    batch method and improved benchmark

--- src/cmd/bench/main.go
+++ src/cmd/bench/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"runtime/pprof"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/richardartoul/tsdb-layer/src/layer"
 )
 
+var (
+	numSeriesFlag  = flag.Int("numSeries", 100000, "number of unique series")
+	batchSizeFlag  = flag.Int("batchSize", 8, "client batch size")
+	numWorkersFlag = flag.Int("numWorkers", 1000, "number of concurrent workers")
+	durationFlag   = flag.Duration("duration", time.Minute, "duration to run the load test")
+)
+
 func main() {
+	flag.Parse()
+
 	tempFile, err := ioutil.TempFile("", "bench_cpu	")
 	if err != nil {
 		panic(err)
@@ -23,67 +34,70 @@ func main() {
 		fmt.Println("cpu profile at:", tempFile.Name())
 	}()
 
-	testCases := []struct {
-		numSeries          int
-		numWritesPerWorker int
-		numWorkers         int
-	}{
-		{
-			numSeries:          1000,
-			numWritesPerWorker: 100,
-			numWorkers:         1000,
-		},
-		{
-			numSeries:          100000,
-			numWritesPerWorker: 100,
-			numWorkers:         1000,
-		},
-		{
-			numSeries:          100000,
-			numWritesPerWorker: 100,
-			numWorkers:         2000,
-		},
-		{
-			numSeries:          100000,
-			numWritesPerWorker: 10,
-			numWorkers:         10000,
-		},
+	var (
+		numSeries  = *numSeriesFlag
+		batchSize  = *batchSizeFlag
+		numWorkers = *numWorkersFlag
+		duration   = *durationFlag
+	)
+	fmt.Println("Running test with arguments:")
+	fmt.Println("    numSeries:", numSeries)
+	fmt.Println("    batchSize:", batchSize)
+	fmt.Println("    numWorkers:", numWorkers)
+	fmt.Println("    duration:", duration)
+
+	layerClient := layer.NewLayer()
+	seriesIDs := make([]string, 0, numSeries)
+	for i := 0; i < numSeries; i++ {
+		seriesIDs = append(seriesIDs, fmt.Sprintf("%s-%d", randomString(20), i))
 	}
 
-	layer := layer.NewLayer()
-	for _, tc := range testCases {
-		fmt.Printf("%+v\n", tc)
-		seriesIDs := make([]string, 0, tc.numSeries)
-		for i := 0; i < tc.numSeries; i++ {
-			seriesIDs = append(seriesIDs, fmt.Sprintf("some-long-ts-id-new-%d", i))
-		}
+	var (
+		wg                 sync.WaitGroup
+		numWritesCompleted int64
+		doneCh             = make(chan struct{})
+	)
+	go func() {
+		time.Sleep(duration)
+		close(doneCh)
+	}()
+	for i := 0; i < numWorkers; i++ {
+		source := rand.NewSource(time.Now().UnixNano())
+		rng := rand.New(source)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
 
-		var (
-			start = time.Now()
-			wg    sync.WaitGroup
-		)
-		for i := 0; i < tc.numWorkers; i++ {
-			source := rand.NewSource(time.Now().UnixNano())
-			rng := rand.New(source)
-			wg.Add(1)
-			go func() {
-				for j := 0; j < tc.numWritesPerWorker; j++ {
-					idx := rng.Intn(tc.numSeries)
-					if err := layer.Write(seriesIDs[idx], time.Unix(0, int64(j)), float64(j)); err != nil {
-						panic(err)
-					}
+			j := 0
+			for {
+				select {
+				case <-doneCh:
+					return
+				default:
 				}
-				wg.Done()
-			}()
-		}
-		wg.Wait()
-
-		var (
-			numWrites = tc.numWorkers * tc.numWritesPerWorker
-			duration  = time.Now().Sub(start)
-			qps       = float64(numWrites) / duration.Seconds()
-		)
-		fmt.Println("Duration: ", duration)
-		fmt.Println("QPS: ", qps)
+				batch := make([]layer.Write, 0, batchSize)
+				for y := 0; y < batchSize; y++ {
+					idx := rng.Intn(numSeries)
+					batch = append(batch, layer.Write{ID: seriesIDs[idx], Timestamp: time.Unix(0, int64(j)), Value: float64(j)})
+				}
+				if err := layerClient.WriteBatch(batch); err != nil {
+					panic(err)
+				}
+				atomic.AddInt64(&numWritesCompleted, int64(batchSize))
+				j++
+			}
+		}()
 	}
+	wg.Wait()
+
+	qps := float64(numWritesCompleted) / duration.Seconds()
+	fmt.Println("QPS: ", qps)
+}
+
+func randomString(len int) string {
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(65 + rand.Intn(25)) //A=65 and Z = 65+25
+	}
+	return string(bytes)
 }
--- src/layer/layer.go
+++ src/layer/layer.go
@@ -12,6 +12,7 @@ import (
 
 type Layer interface {
 	Write(id string, timestamp time.Time, value float64) error
+	WriteBatch(writes []Write) error
 	Read(id string) (encoding.Decoder, error)
 }
 
@@ -33,85 +34,103 @@ type timeSeriesMetadata struct {
 	LastByte byte
 }
 
+type Write struct {
+	ID        string
+	Timestamp time.Time
+	Value     float64
+}
+
 func (l *layer) Write(id string, timestamp time.Time, value float64) error {
+	// TODO: Don't allocate
+	return l.WriteBatch([]Write{Write{ID: id, Timestamp: timestamp, Value: value}})
+}
+
+func (l *layer) WriteBatch(writes []Write) error {
 	_, err := l.db.Transact(func(tr fdb.Transaction) (interface{}, error) {
-		var (
-			metadataKey    = newTimeseriesMetadataKeyFromID(id)
-			dataKey        = newTimeseriesDataKeyFromID(id)
-			metadataFuture = tr.Get(metadataKey)
-			// TODO(rartoul): Proper error handling instead of Must()
-			metadataBytes = metadataFuture.MustGet()
-		)
+		metadataFutures := make([]fdb.FutureByteSlice, 0, len(writes))
+		for _, w := range writes {
+			metadataKey := newTimeseriesMetadataKeyFromID(w.ID)
+			metadataFuture := tr.Get(metadataKey)
+			metadataFutures = append(metadataFutures, metadataFuture)
+		}
 
-		var (
-			metaValue  timeSeriesMetadata
-			dataAppend []byte
-			enc        = encoding.NewEncoder()
-		)
-		if len(metadataBytes) == 0 {
-			// Never written.
-			enc := encoding.NewEncoder()
-			if err := enc.Encode(timestamp, value); err != nil {
-				return nil, err
-			}
-
-			metaValue = timeSeriesMetadata{
-				State: enc.State(),
-			}
-
-			b := enc.Bytes()
-			if len(b) > 1 {
-				dataAppend = enc.Bytes()[:len(b)-1]
-			}
-		} else {
-			// TODO(rartoul): Don't use JSON.
-			if err := json.Unmarshal(metadataBytes, &metaValue); err != nil {
-				return nil, err
-			}
-
-			// Has been written before, restore encoder state.
-			if err := enc.Restore(metaValue.State); err != nil {
-				return nil, err
-			}
-
-			if err := enc.Encode(timestamp, value); err != nil {
-				return nil, err
-			}
-
-			// Ensure new state gets persisted.
+		for i, f := range metadataFutures {
 			var (
-				newState = enc.State()
-				b        = enc.Bytes()
+				w           = writes[i]
+				metadataKey = newTimeseriesMetadataKeyFromID(w.ID)
+				// TODO: Error handling
+				metadataBytes = f.MustGet()
+				metaValue     timeSeriesMetadata
+				dataAppend    []byte
+				enc           = encoding.NewEncoder()
 			)
-			if len(b) == 0 {
-				return nil, errors.New("encoder bytes was length zero")
-			}
-			if len(b) == 1 {
-				// The existing last byte was modified without adding any additional bytes. The last
-				// byte is always tracked by the state so there is nothing to append here.
-			}
-			if len(b) > 1 {
-				// The last byte will be kept track of by the state, but any bytes preceding it are
-				// new "complete" bytes which should be appended to the compressed stream.
-				dataAppend = b[:len(b)-1]
-			}
-			metaValue.LastByte = b[len(b)-1]
-			metaValue.State = newState
+			if len(metadataBytes) == 0 {
+				// Never written.
+				enc := encoding.NewEncoder()
+				if err := enc.Encode(w.Timestamp, w.Value); err != nil {
+					return nil, err
+				}
 
+				metaValue = timeSeriesMetadata{
+					State: enc.State(),
+				}
+
+				// TODO: Should lastByte be set here too?
+				b := enc.Bytes()
+				if len(b) > 1 {
+					dataAppend = enc.Bytes()[:len(b)-1]
+				}
+			} else {
+				// TODO(rartoul): Don't use JSON.
+				if err := json.Unmarshal(metadataBytes, &metaValue); err != nil {
+					return nil, err
+				}
+
+				// Has been written before, restore encoder state.
+				if err := enc.Restore(metaValue.State); err != nil {
+					return nil, err
+				}
+
+				if err := enc.Encode(w.Timestamp, w.Value); err != nil {
+					return nil, err
+				}
+
+				// Ensure new state gets persisted.
+				var (
+					newState = enc.State()
+					b        = enc.Bytes()
+				)
+				if len(b) == 0 {
+					return nil, errors.New("encoder bytes was length zero")
+				}
+				if len(b) == 1 {
+					// The existing last byte was modified without adding any additional bytes. The last
+					// byte is always tracked by the state so there is nothing to append here.
+				}
+				if len(b) > 1 {
+					// The last byte will be kept track of by the state, but any bytes preceding it are
+					// new "complete" bytes which should be appended to the compressed stream.
+					dataAppend = b[:len(b)-1]
+				}
+				metaValue.LastByte = b[len(b)-1]
+				metaValue.State = newState
+			}
+
+			// TODO(rartoul): Don't use JSON.
+			newMetadataBytes, err := json.Marshal(&metaValue)
+			if err != nil {
+				return nil, err
+			}
+
+			tr.Set(metadataKey, newMetadataBytes)
+			// TODO(rartoul): Ensure it fits and if not split into new keys.
+			dataKey := newTimeseriesDataKeyFromID(w.ID)
+			tr.AppendIfFits(dataKey, dataAppend)
 		}
-
-		// TODO(rartoul): Don't use JSON.
-		newMetadataBytes, err := json.Marshal(&metaValue)
-		if err != nil {
-			return nil, err
-		}
-
-		tr.Set(metadataKey, newMetadataBytes)
-		// TODO(rartoul): Ensure it fits and if not split into new keys.
-		tr.AppendIfFits(dataKey, dataAppend)
 
 		return nil, nil
 	})
+
 	if err != nil {
 		return err
 	}
