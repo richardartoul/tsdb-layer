d27cbc5a96c8f2ba898801a8631cbbd08457b874
Richard Artoul <rartoul@uber.com>
2019-06-08 12:43:16 -0400

    add read layer to buffer + tests

--- src/layer/rawblock/buffer.go
+++ src/layer/rawblock/buffer.go
@@ -9,6 +9,7 @@ import (
 
 type Buffer interface {
 	Write(writes []layer.Write) error
+	Read(id string) (encoding.MultiDecoder, bool, error)
 }
 
 type buffer struct {
@@ -40,4 +41,26 @@ func (b *buffer) Write(writes []layer.Write) error {
 	}
 
 	return nil
+}
+
+func (b *buffer) Read(id string) (encoding.MultiDecoder, bool, error) {
+	encoders, ok := b.encoders[id]
+	if !ok {
+		return nil, false, nil
+	}
+
+	decs := encodersToDecoders(encoders)
+	multiDec := encoding.NewMultiDecoder()
+	multiDec.Reset(decs)
+	return multiDec, true, nil
+}
+
+func encodersToDecoders(encs []encoding.Encoder) []encoding.Decoder {
+	decs := make([]encoding.Decoder, 0, len(encs))
+	for _, enc := range encs {
+		dec := encoding.NewDecoder()
+		dec.Reset(enc.Bytes())
+		decs = append(decs, dec)
+	}
+	return decs
 }
---
+++ src/layer/rawblock/buffer_test.go
@@ -0,0 +1,71 @@
+package rawblock
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/richardartoul/tsdb-layer/src/layer"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	testID = "test-id"
+)
+
+type testValue struct {
+	timestamp time.Time
+	value     float64
+}
+
+type bufferWriteReadTestCase struct {
+	title string
+	vals  []testValue
+}
+
+func TestBufferWriteRead(t *testing.T) {
+	testCases := []bufferWriteReadTestCase{
+		{
+			title: "in order values",
+			vals:  []testValue{{timestamp: time.Unix(0, 0), value: 0}, {timestamp: time.Unix(1, 0), value: 1}},
+		},
+		// {
+		// 	title: "out of order values",
+		// 	vals:  []testValue{{timestamp: time.Unix(1, 0), value: 1}, {timestamp: time.Unix(0, 0), value: 0}},
+		// },
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			buffer := NewBuffer()
+			writes := []layer.Write{}
+			for _, val := range tc.vals {
+				writes = append(
+					writes,
+					layer.Write{
+						ID:        testID,
+						Timestamp: val.timestamp,
+						Value:     val.value})
+			}
+			require.NoError(t, buffer.Write(writes))
+
+			multiDec, ok, err := buffer.Read(testID)
+			require.NoError(t, err)
+			require.True(t, ok)
+
+			i := 0
+			for multiDec.Next() {
+				currT, currV := multiDec.Current()
+				require.True(
+					t,
+					tc.vals[i].timestamp.Equal(currT),
+					fmt.Sprintf("expected %s but got %s", tc.vals[i].timestamp.String(), currT.String()))
+				require.Equal(t, tc.vals[i].value, currV)
+				i++
+			}
+			require.NoError(t, multiDec.Err())
+			require.Equal(t, len(tc.vals), i)
+		})
+	}
+}
